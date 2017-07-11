b8a02e92c7cb95a22e27de239a18c198b6a2ae81
Jorge <jlmadrid360@gmail.com>
2017-07-11 10:23:05 -0500

    Upgrade version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PubNux.Mixfile do
   def project do
     [
       app: :pubnux,
-      version: "0.1.0",
+      version: "0.0.2",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -33,6 +33,7 @@ defmodule PubNux.Mixfile do
       {:exvcr, "~> 0.8", only: :test},
       {:dialyxir, "~> 0.4.1", only: :dev},
       {:credo, "~> 0.7.4", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
 
