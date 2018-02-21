289b3d8cc82561e87cb5f810cd6d2566a6334001
Michel Perez <michel.ingesoft@gmail.com>
2018-02-21 15:55:18 -0500

    fixing guard

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
 config :pubnux,
-  publish_key: "pub-c-e72030bd-19b4-46d8-a29b-e1bf93dfa3c1",
-  subscription_key: "sub-c-ce898f06-7608-11e7-81e6-0619f8945a4f",
+  publish_key: "pub-c-e72030bd",
+  subscription_key: "sub-c-ce898f06",
   is_ssl: false
